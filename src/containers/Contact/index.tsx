9b55eb0842c9ea05434c69b62b13243a03be6e4a
ANA BUENO <anabuenogomes@hotmail.com>
2025-01-06 01:24:47 +0000

    creating the connection with the email

--- src/containers/Contact/index.tsx
+++ src/containers/Contact/index.tsx
@@ -1,5 +1,5 @@
-import Paragraph from '../../components/Paragraph'
-import Title from '../../components/Title'
+import { useState } from 'react'
+import emailjs from 'emailjs-com'
 import {
   CardContact,
   ButtonEmail,
@@ -12,78 +12,165 @@ import { Description } from '../Sidebar/styles'
 import { FaLocationDot } from 'react-icons/fa6'
 import { FaPhoneAlt } from 'react-icons/fa'
 import { IoMdMail } from 'react-icons/io'
+import Paragraph from '../../components/Paragraph'
+import Title from '../../components/Title'
 
 type Props = {
   theme: string
 }
 
-const Contact = ({ theme }: Props) => (
-  <section>
-    <Title fontSize={16}>Contact me</Title>
-    <ContactContainer>
-      <CardContact>
-        <div>
-          <div style={{ display: 'flex', alignItems: 'center' }}>
-            <FaLocationDot
-              color={theme === 'dark' ? 'white' : 'black'}
-              size={15}
-              style={{ marginRight: '10px' }}
-            />
-            <Paragraph type="principal" fontSize={15}>
-              Location:
-            </Paragraph>
-          </div>
-          <Description type="secundary" fontSize={13}>
-            Lisbon, Portugal
-          </Description>
-        </div>
+const Contact = ({ theme }: Props) => {
+  const [subject, setSubject] = useState('')
+  const [email, setEmail] = useState('')
+  const [message, setMessage] = useState('')
+  const [error, setError] = useState('')
+  const [subjectError, setSubjectError] = useState(false)
+  const [emailError, setEmailError] = useState(false)
+  const [messageError, setMessageError] = useState(false)
 
-        <div>
-          <div style={{ display: 'flex', alignItems: 'center' }}>
-            <FaPhoneAlt
-              color={theme === 'dark' ? 'white' : 'black'}
-              size={15}
-              style={{ marginRight: '10px' }}
-            />
-            <Paragraph type="principal" fontSize={15}>
-              Phone:
-            </Paragraph>
-          </div>
-          <Description type="secundary" fontSize={13}>
-            +351 932 338 188
-          </Description>
-        </div>
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault()
 
-        <div>
-          <div style={{ display: 'flex', alignItems: 'center' }}>
-            <IoMdMail
-              color={theme === 'dark' ? 'white' : 'black'}
-              size={15}
-              style={{ marginRight: '10px' }}
-            />
-            <Paragraph type="principal" fontSize={15}>
-              Email:
-            </Paragraph>
-          </div>
-          <Description type="secundary" fontSize={13}>
-            anabuenogomes@hotmail.com
-          </Description>
-        </div>
-      </CardContact>
+    let hasError = false
+    if (!subject) {
+      setSubjectError(true)
+      hasError = true
+    } else {
+      setSubjectError(false)
+    }
 
-      <CardContact style={{ paddingTop: '40px' }}>
-        <InputField type="text" placeholder="Subject" />
-        <InputField type="email" placeholder="Email" />
-        <TextArea placeholder="Message" />
-        <ButtonEmail type="submit">Send e-mail</ButtonEmail>
-      </CardContact>
-    </ContactContainer>
-    <FooterStyle>
-      <Paragraph type="principal">
-        © 2025 Ana Bueno. All rights reserved.
-      </Paragraph>
-    </FooterStyle>
-  </section>
-)
+    if (!email) {
+      setEmailError(true)
+      hasError = true
+    } else {
+      setEmailError(false)
+    }
+
+    if (!message) {
+      setMessageError(true)
+      hasError = true
+    } else {
+      setMessageError(false)
+    }
+
+    if (hasError) {
+      return
+    }
+
+    setError('')
+
+    emailjs
+      .sendForm(
+        'service_d9pti69',
+        'template_tpp4cd6',
+        e.target as HTMLFormElement,
+        '1nWRalWpODWiAltIz'
+      )
+      .then(
+        (result) => {
+          console.log(result.text)
+
+          setSubject('')
+          setEmail('')
+          setMessage('')
+        },
+        (error) => {
+          console.log(error.text)
+          alert('Failed to send message.')
+        }
+      )
+  }
+
+  return (
+    <section>
+      <Title fontSize={16}>Contact me</Title>
+      <ContactContainer>
+        <CardContact>
+          <div>
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+              <FaLocationDot
+                color={theme === 'dark' ? 'white' : 'black'}
+                size={15}
+                style={{ marginRight: '10px' }}
+              />
+              <Paragraph type="principal" fontSize={15}>
+                Location:
+              </Paragraph>
+            </div>
+            <Description type="secundary" fontSize={13}>
+              Lisbon, Portugal
+            </Description>
+          </div>
+
+          <div>
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+              <FaPhoneAlt
+                color={theme === 'dark' ? 'white' : 'black'}
+                size={15}
+                style={{ marginRight: '10px' }}
+              />
+              <Paragraph type="principal" fontSize={15}>
+                Phone:
+              </Paragraph>
+            </div>
+            <Description type="secundary" fontSize={13}>
+              +351 932 338 188
+            </Description>
+          </div>
+
+          <div>
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+              <IoMdMail
+                color={theme === 'dark' ? 'white' : 'black'}
+                size={15}
+                style={{ marginRight: '10px' }}
+              />
+              <Paragraph type="principal" fontSize={15}>
+                Email:
+              </Paragraph>
+            </div>
+            <Description type="secundary" fontSize={13}>
+              anabuenogomes@hotmail.com
+            </Description>
+          </div>
+        </CardContact>
+
+        <CardContact style={{ paddingTop: '40px' }}>
+          <form onSubmit={handleSubmit}>
+            <InputField
+              type="text"
+              name="subject"
+              value={subject}
+              onChange={(e) => setSubject(e.target.value)}
+              placeholder="Subject"
+              style={{ borderColor: subjectError ? 'red' : '' }}
+            />
+            <InputField
+              type="email"
+              name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Email"
+              style={{ borderColor: emailError ? 'red' : '' }}
+            />
+            <TextArea
+              name="message"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+              placeholder="Message"
+              style={{ borderColor: messageError ? 'red' : '' }}
+            />
+            <ButtonEmail type="submit">Send e-mail</ButtonEmail>
+          </form>
+        </CardContact>
+      </ContactContainer>
+      <FooterStyle>
+        <Paragraph type="principal">
+          © 2025 Ana Bueno. All rights reserved.
+        </Paragraph>
+      </FooterStyle>
+    </section>
+  )
+}
 
 export default Contact
